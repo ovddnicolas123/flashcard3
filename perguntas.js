e196f601a3f056d82cf04258d2d9db1410b90502
ovddnicolas123 <spell.dirty.dancing@gmail.com>
2024-11-28 11:45:24 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -16,5 +16,21 @@ criaCartao(
     'Uma função é um bloco de código que executa alguma tarefa'
 )
 
+criaCartao(
+    'Programação',
+    'Quais sao os 3 pokemons iniciais?',
+    'bubassaur, squire e charmander'
+)
 
+criaCartao(
+    'Programação',
+    'quanto é 1+1?',
+    '2'
+)
+
+criaCartao(
+    'Programação',
+    'Com o que o Brasil ajuda na acenssao dos furrys',
+    'Nada XD'
+)
 
